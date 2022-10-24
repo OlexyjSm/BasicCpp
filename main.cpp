ef8bf352f3cbbc4f6958e40b2cb781f5ca3bc8fb
Smyatsky <mamaiko471@gmail.com>
2022-10-24 19:47:47 +0300

    Done_Task_#1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,51 @@
+//---------------------------------------------------------------------------------------------------------#
+//Task 1. Display one verse of your favorite poem or song on the screen, indicating the author or artist.  |
+//Use escape sequences for formatting.                                                                     |
+//---------------------------------------------------------------------------------------------------------#
+
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
+	cout << "\n\n" << endl;
+
+
+
+	cout << "\t\t\t\t \"Hlian na zirochky in the sky" << endl;
+	
+	cout << "\t\t\t\tNaidy sobi svoiu i ne puskai" << endl;
+	
+	cout << "\t\t\t\tI navit v samyi temnyi chas" << endl;
+	
+	cout << "\t\t\t\tOt tobi moie pleche," << endl;
+	
+	cout << "\t\t\t\tOt tobi moia ruka" << endl;
+	
+	cout << "\t\t\t\tHlian na zirochky in the sky" << endl;
+	
+	cout << "\t\t\t\tNaidy sobi svoiu i ne puskai" << endl;
+	
+	cout << "\t\t\t\tI navit v samyi temnyi chas" << endl;
+	
+	cout << "\t\t\t\tOt tobi moie pleche," << endl;
+	
+	cout << "\t\t\t\tOt tobi moia ruka" << endl;
+	
+	cout << "\t\t\t\tTryvohy i liubov," << endl;
+	
+	cout << "\t\t\t\tMriia u vohni" << endl;
+	
+	cout << "\t\t\t\tDavai ya budu svitytys tobi, a ty meni\"" << endl;
+	
+	cout << "\n\n\t\t\tNazva pisni: \"Zirochky\"" << endl;
+	
+	cout << "\n\n\t\t\t\Vykonavets: Thekomakoma (feat. Just Nelson)" << endl;
+	
+	
+	
+	cout << "\n\n" << endl;
+
 	return 0;
 }
