c7bdc1d964b03dd5779e5dec71baf1ffa8886af4
Smyatsky <mamaiko471@gmail.com>
2022-10-26 16:47:52 +0300

    Done_Task_#2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
+//---------------------------------------------------------------------------------#
+//Task 2. Display the poem "Every hunter wants..."                                 |
+//so that each "color" starts with a new line and the corresponding number of tabs.|
+//---------------------------------------------------------------------------------#
+
+
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
-	return 0;
+	    cout << "Every\n";
+		cout << "\t hunter\n";
+		cout << "\t\t wants \n";
+		cout << "\t\t\t to\n";
+		cout << "\t\t\t\t know \n";
+		cout << "\t\t\t\t\t\ where\n";
+		cout << "\t\t\t\t\t\t sits \n";
+		cout << "\t\t\t\t\t\t\t the \n";
+		cout << "\t\t\t\t\t\t\t\t pheasant.\n";
+
+		return 0;
 }
