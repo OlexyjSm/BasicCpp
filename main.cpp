d3ee508638e2ee27e56124f8bda5599247a49aba
Smyatsky <mamaiko471@gmail.com>
2022-10-26 19:08:33 +0300

    Done_Task_#3

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
+//---------------------------------------------------------------------------------#
+//Task 3. Write an ad for the sale of somethingand enter it on the screen as it is |
+//should have been printed.                                                        |
+//---------------------------------------------------------------------------------#
+
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
-	return 0;
+	cout << "\n";
+	
+	    cout << "\t\t\t\t\t\tJeep Grand Cherokee for sale" << endl;
+	    cout << "\t\t\t1,300,634 UAH\n";
+	    cout << "\tCleared: Yes";
+		cout << "\tThe car was driven from: USA";
+		cout << "\tModel: Grand Cherokee";
+		cout <<  "\tRelease year: 2018\n";
+		cout << "\tBody type: SUV / Crossover";
+		cout << "\tColor: White";
+		cout << "\tGearbox: Automatic";
+		cout << "\tType of fuel: Gasoline\n";
+		cout << "\tType of drive: Full";
+		cout << "\tPower: 290 hp.";
+		cout << "\tFuel consumption: 13 l/100 km\n" << endl;
+		cout << "\tJeep Grand Cherokee for sale. 2018 with a mileage of 42,000 km.\n" <<endl;
+		cout << "\tFully serviced.Second owner since January 2021. There was minor damage.\n";
+		
+    cout << "\n\n";
+				return 0;
 }
