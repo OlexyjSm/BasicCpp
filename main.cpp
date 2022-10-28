5b209ad36f940e16a6cd6259fa3026a293b29de5
Smyatsky <mamaiko471@gmail.com>
2022-10-28 17:19:13 +0300

    Done_Task_#1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
+//---------------------------------------------------------------------------------------------#
+//Task 1. Create an application that displays information about a book in the following format:|
+//---------------------------------------------------------------------------------------------#
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
+
+   
+	cout << "\t\t\t\tName\t\t: The war and the peace" << endl;
+	
+	cout << "\t\t\t\tAvtor \t\t: L.N.Tolstoj" << endl;
+  
+	cout << "\t\t\t\tIzdatelstvo\t: Piter" << endl;
+   
+	cout << "\t\t\t\tPages \t\t: 500." << endl;
+	
 	return 0;
 }
