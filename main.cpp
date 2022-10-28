39af22bb31b1869947fc06fa2000881ba882d31e
Smyatsky <mamaiko471@gmail.com>
2022-10-28 18:25:49 +0300

    Done_Task_#2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
+//----------------------------------------------------------#
+//Task 2. Create an application that outputs to the console |
+//phrase :                                                  |
+//Accompany each new line with a triple sound signal        |
+//----------------------------------------------------------#
+
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
+	   
+	cout << "\t\tKraj lukomor’a dub zelenyj\a" << endl;
+		
+	cout << "\t\tI zolotyj lancyuh na nim:" << endl;
+		
+	cout << "\t\tShodnya, shonochi kit uchenyj" << endl;
+		
+	cout << "\t\tNa lancyuhu kruzhl’aje tim" << endl;
+	
 	return 0;
 }
