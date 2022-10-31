1c8be04e0beb9f59fe7674bb57bc056c73e195a8
Smyatsky <mamaiko471@gmail.com>
2022-10-31 18:57:43 +0200

    Done_Task_3

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int main()
+int main(char*)
 {
+	
+	cout << (char)201 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)187 <<endl;
+	
+	cout << (char)186 <<"\t\t\t\t\t\t\t\t" <<(char)186 << endl;
+	
+	cout<<(char)186 << "\t\t\t\tPory Roky" << "\t\t\t" <<(char)186 << endl;
+	
+	cout << (char)186 << "\t\t\t\t\t\t\t\t" << (char)186 << endl;
+	
+	cout << (char)186 << "\t\t\t\t\t\t\t\t" << (char)186 << endl;
+	
+	cout << (char)204 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)203<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)203 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)203 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)185 << endl;
+	
+	cout << (char)186 << "\t\t"<<(char)186 << "\t\t"<<(char)186<< "\t\t" << (char)186<< "\t\t" << (char)186 << endl;
+	
+	cout << (char)186 << "\tZuma" << "\t" << (char)186 <<"\tVesna" << "\t" <<(char)186 << "\tLito" << "\t" << (char)186 << "\tOsin" << "\t" << (char)186 << endl;
+	
+	cout << (char)186 << "\t\t" << (char)186<<"\t\t" << (char)186<<"\t\t" << (char)186<< "\t\t" << (char)186;
+	
+	cout <<"\n" << (char)200 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)202 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205<<(char)202 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)202 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)188 << endl;
+	
 	return 0;
 }
