c5a14494f78bf47b0e2a49f64d37407f542b6d00
Smyatsky <mamaiko471@gmail.com>
2022-11-06 15:55:12 +0200

    Done_Task_1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
+#define _USE_MATH_DEFINES
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
+	int t, s, v;
+	cout << "Enter the distance to the airport (in km): ";
+	cin >> s;
+	cout << "Enter the time you need to arrive (in hours): ";
+	cin >> t;
+	v = s / t;
+	cout << "You need to drive at speed: " << v << " km/h" << endl;
+	
+	system("pause");
+
 	return 0;
 }
