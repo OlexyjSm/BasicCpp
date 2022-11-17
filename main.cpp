6b9bc6aba1d3556a32efbd6c7f70a61dd4c53cd4
Smyatsky <mamaiko471@gmail.com>
2022-11-17 19:32:03 +0200

    Done_Task_5

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <locale.h>
+#include <windows.h>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-	return 0;
-}
+    int s;
+
+        cout << "Enter seconds:";
+        cin >> s;
+  
+        cout << "Until the end of the working day: " << 8 - (s / 60 / 60) << " hours " << endl;
+
+
+
+        return 0;
+ }
