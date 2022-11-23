22d47a8bfa6ad97152f1f2aa4351b2db78c5d8c7
Smyatsky <mamaiko471@gmail.com>
2022-11-23 23:23:05 +0200

    Done_Task_2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <cmath>
+
+
 using namespace std;
 
 int main()
 {
+	
+	
+	int x = 0;
+
+	int y = 0;
+
+	cout << "Enter x: ";
+	
+	cin >> x;
+
+	cout << "Enter y: ";
+
+	cin >> y;
+
+	cout << "Result :"<< powl(x, y);
+
 	return 0;
 }
