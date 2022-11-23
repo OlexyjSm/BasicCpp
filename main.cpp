5350c527189a98319371c59cd2c76bf1025e5d85
Smyatsky <mamaiko471@gmail.com>
2022-11-23 22:44:25 +0200

    Done_Task_1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
+//--------------------------------------------------------------------#
+//Task 1. Write a program that calculates the sum of integers         |
+//numbers from a to 500 (the value of a is entered from the keyboard).|
+//--------------------------------------------------------------------#
+
+
 #include <iostream>
+#include <locale.h>
+#include <windows.h>
+#include <cmath>
 using namespace std;
 
 int main()
 {
+
+	int a;
+	
+	cout << "Enter number: ";
+	
+	cin >> a;
+	
+	cout << (a + 500) / 2 * (500 - a + 1);
+	
+	cout << endl;
+
+	system("PAUSE");
+	
+	
 	return 0;
+
+
 }
