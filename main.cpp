521b30583e3f64d5cc669be5c5af87c446f0e982
Smyatsky <mamaiko471@gmail.com>
2022-12-01 23:04:20 +0200

    Done_Task_4

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
+	
+	long long p = 1;
+	
+	int a;
+	
+	cin >> a;
+	
+	for (int i = a; i <= 20; i++)
+	
+		p *= i;
+	
+	cout << p << endl;
+	
+	system("pause");
+	
 	return 0;
 }
