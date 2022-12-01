f4dbc5cee55bcd0a39d6972065cdc0c63c2c4b15
Smyatsky <mamaiko471@gmail.com>
2022-12-02 00:05:08 +0200

    Done_task_5

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
+
+	int num;
+
+	cout << "Enter number: ";
+
+	cin >> num;
+
+	if (num > 0 && num <= 10)
+	{
+
+		for (int k = 1; k <= 10; k++)
+
+			cout << num << "x" << k << "=" << num * k << endl;
+
+	}
+	else
+
+		cout << "Wrong input" << endl;
+	
+	system("PAUSE");
+	
+	
 	return 0;
 }
