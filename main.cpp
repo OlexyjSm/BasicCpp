5a74b6327cc83f85683c199d8082f74e40a3a953
Smyatsky <mamaiko471@gmail.com>
2022-12-01 22:04:39 +0200

    Done_Task_3

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
 int main()
 {
-	return 0;
+    int a, b;
+
+    float sr = 0;
+    
+    cout << "a = ";  cin >> a;
+    
+    cout << "b = ";  cin >> b;
+    
+    for (int i = a; i <= b; i++) sr += i;
+    
+    sr = sr / (1000 - 1);
+    
+    cout << sr << endl;
+    
+    system("pause");
+    
+    return 0;
 }
