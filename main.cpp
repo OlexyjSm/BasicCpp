af277a7c77f36765d069159d60a29a0a7ece3d40
Smyatsky <mamaiko471@gmail.com>
2022-12-09 17:45:11 +0200

    Done_Task_6

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
+//---------------------------------------------------#
+//Task 8. The user enters any integer.               |
+//It is necessary to remove all digits 3 and 6 from  |
+//this integer and output back to the screen.        |
+//---------------------------------------------------#
+
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
+    int a, res;
+    double sum = 0;
+    int k = 0;
+    cout << "Enter number: ";
+    cin >> a;
+    while (a != 0) {
+        res = a % 10;
+        if (res != 3 && res != 6) {
+            sum += res*pow(10, k);
+            k++;
+        }
+        a /= 10;
+    }
+    cout << "The resulting number without 3 and 6: " << sum<<endl;
+
+
+    system("pause");
+
 	return 0;
 }
