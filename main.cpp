1443ff398eb439422e7d76031aa91619cf612b7f
Smyatsky <mamaiko471@gmail.com>
2023-03-06 14:22:04 +0200

    Done_Task_#2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
 using namespace std;
-
-int main()
+const int SIZE = 10;
+double averege(int array[][SIZE])
 {
-	return 0;
+    double SUMMA = 0;
+    for (int i = 0; i < SIZE; i++)
+        for (int j = 0; j < SIZE; j++)
+            SUMMA += array[i][j];
+    return SUMMA / (SIZE * SIZE);
+}
+void Print(int array[][SIZE])
+{
+    cout << " Array" << endl;
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+            cout << array[i][j] << "\t";
+        cout << endl;
+    }
+}
+void Set(int array[][SIZE])
+{
+    for (int i = 0; i < SIZE; i++)
+        for (int j = 0; j < SIZE; j++)
+            array[i][j] = rand() % 100;
+}
+
+void main()
+{
+    int a[SIZE][SIZE];
+    Set(a);
+    Print(a);
+    cout << "Average = " << averege(a) << endl;
+    system("pause");
+
 }
