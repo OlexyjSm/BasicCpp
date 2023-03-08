1934fa0e7b16744187a9622f29c9e4f9b94f742f
Smyatsky <mamaiko471@gmail.com>
2023-03-08 15:46:33 +0200

    Done_Task_#2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
+//---------------------------------------------------#
+//Write a recursive finding function                 |
+//the greatest common divisor of two integers.       |
+//---------------------------------------------------#
+
 #include <iostream>
 using namespace std;
+int NOD(int a, int b)
+{
+    if (b == 0)
+        return a;
+    if (a > b)
+        return NOD(b, a % b);
+    else
+        return NOD(a, b % a);
+}
 
 int main()
 {
-	return 0;
+    setlocale(0, "");
+    int a, b;
+    cout << "Введите 2 числа: " << endl;
+    cin >> a >> b;
+    cout << "Наибольльший общий делитель равен: " << NOD(b, a % b) << endl;
+    cin.get();
+    cin.get();
+    return 0;
 }
