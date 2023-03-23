fe5813eca49fffae8aa83eb37db951c7524117d1
Smyatsky <mamaiko471@gmail.com>
2023-03-23 16:21:54 +0200

    Done_Task_#2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
+//---------------------------------------------------------------------------#
+//Task 2. Using a pointer to an array of integers,                                   |
+//reverse the order of the elements of the array.                            |
+//Use the arithmetic of pointers in the program to move through the array,   |
+//as well as the dereferencing operator.                                     |
+//---------------------------------------------------------------------------#
+
+
 #include <iostream>
 using namespace std;
 
-int main()
+void main()
 {
-	return 0;
+    const int size = 5;
+    int arr[size] = { 5, 7, -8, 3, 9 }, temp;
+    int* start = arr, * end = &arr[size - 1];
+
+    while (start != end)
+    {
+        temp = *start;
+        *start = *end;
+        *end = temp;
+
+        start++;
+        end--;
+        cout << temp;
+    }
 }
